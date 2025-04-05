9b4d5fc5c83fe0be71622809302de706eeff56d1
FelipeMrtnsDev <juninpernadecano@gmail.com>
2025-04-04 23:35:08 -0400

    cors fixed v2

--- src/server.js
+++ src/server.js
@@ -19,9 +19,10 @@ const allowedOrigins = [
 ];
 
 app.use(express.json())
-app.use(cors());
 app.use(cors({
     origin: allowedOrigins,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
